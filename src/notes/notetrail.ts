cfebfb93dabec2d161c7165728601fd5d1f9d165
StormPacer <nejc.zivic@gmail.com>
2022-03-22 18:42:27 +0100

    Fixed note trails

--- src/notes/notetrail.ts
+++ src/notes/notetrail.ts
@@ -1,15 +1,21 @@
 import * as Remapper from 'swifter_remapper'
 import Random from '../internal/random';
 
-function InternalNoteTrail(filterednotes: Remapper.Note[], length: number = 5) {
+function InternalNoteTrail(filterednotes: Remapper.Note[], length: number = 5, type: string = "arrow") {
 	filterednotes.forEach(note => {
-		for (let i = 1; i < length; i++) {
+        note.noteGravity = false;
+        note.noteLook = false;
+        note.spawnEffect = false;
+		for (let i = 1; i <= length; i++) {
 			let dupe = new Remapper.Note(note.time, note.type, note.direction, note.position);
+            dupe.noteGravity = false;
+            dupe.noteLook = false;
+            dupe.spawnEffect = false;
 			dupe.fake = true;
-			dupe.animation._position = [[Random(-0.2, 0.2), 0, i * 2, 0], [0, 0, 0, 0.3]];
+			dupe.animation._position = [[Random(-0.2, 0.2), 0, i * 4, 0.4], [0, 0, 0, 0.7]];
 			dupe.animation._localRotation = [[Random(0, 180), Random(0, 180), Random(0, 180), 0], [0, 0, 0, 0.3]];
-			dupe.animation._dissolve = [[0.1, 0], [0, 1, Remapper.EASE.IN_OUT_BOUNCE]];
-			dupe.animation._dissolveArrow = [[0.1, 0], [0, 1, Remapper.EASE.IN_OUT_BOUNCE]];
+			if (type == "block") dupe.animation._dissolve = [[0.1, 0], [0, 1, Remapper.EASE.IN_OUT_CUBIC]];
+			if (type == "arrow") dupe.animation._dissolveArrow = [[0.1, 0]];
 			dupe.push();
 		}
 	})
@@ -20,26 +26,29 @@ function InternalNoteTrail(filterednotes: Remapper.Note[], length: number = 5) {
  * @param startBeat The beat to start the effect on.
  * @param endBeat The beat to end the effect on.
  * @param length The length of the trail.
+ * @param type What the effect should use for the trail, it can be "arrow" or "block"
  * @author cal117
  */
-function NoteTrail(startBeat: number, endBeat: number, length: number = 5) {
+function NoteTrail(startBeat: number, endBeat: number, length: number = 5, type: string = "arrow") {
     const filterednotes = Remapper.activeDiff.notes.filter(note => note.time >= startBeat && note.time <= endBeat);
-    InternalNoteTrail(filterednotes, length);
+    InternalNoteTrail(filterednotes, length, type);
 }
 
 /**
  * Spawns a trail behind a note - similar to Somewhere Out There
  * @param track The track to apply the effect on.
  * @param length The length of the trail.
+ * @param type What the effect should use for the trail, it can be "arrow" or "block"
  * @author cal117
  */
-function NoteTrailTrack(track: string, length: number = 5) {
+function NoteTrailTrack(track: string, length: number = 5, type: string = "arrow") {
     const filterednotes = Remapper.activeDiff.notes.filter(note => {
         if(!note.customData) note.customData = {};
         if(Array.isArray(note.customData._track)) return note.customData._track.includes(track);
         else if (note.customData._track == track) return true;
         else return false;
     });
+    InternalNoteTrail(filterednotes, length, type);
 }
 
 export default {NoteTrail, NoteTrailTrack}
