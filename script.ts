54a212806bd9dbd38e7d9664a2ad73370ccddb64
nasafrasa <86539501+Nasafrasa@users.noreply.github.com>
2022-08-25 11:25:20 -0700

    why cant i just fix all the problems in one commit ;-;

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
-import { Difficulty } from "https://deno.land/x/remapper@2.0.0/src/mod.ts" // MAKE SURE THIS IS ON THE LATEST REMAPPER VERSION!!!!!!!!!
-import {  } from "./nootils/index.ts";
+import { Difficulty } from "https://deno.land/x/remapper@2.0.2/src/mod.ts" // Keep on 2.0.2 for now
+import {  } from "./nootils/index.ts"; // Import nootil classes here!
 
-const map = new Difficulty("ExpertPlusLawless", "ExpertPlusStandard");
+const map = new Difficulty("ExpertPlusLawless.dat", "ExpertPlusStandard.dat");
 
 // SCRIPT
 
