b81597f56e08cbe3f50b4c47067aa92f4b225913
nasafrasa <86539501+Nasafrasa@users.noreply.github.com>
2022-08-25 11:52:42 -0700

    ;-; balls

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 import { Difficulty } from "https://deno.land/x/remapper@2.0.2/src/mod.ts" // Keep on 2.0.2 for now
-import {  } from "./nootils/index.ts" // Import nootil classes here! More Information here: https://github.com/Nasafrasa/nootils-Remapper-Setup/wiki
+import {  } from "./nootils/index.ts" /* Import nootil classes here! More Information here: https://github.com/Nasafrasa/nootils-Remapper-Setup/wiki */
 
 const map = new Difficulty("ExpertPlusLawless.dat", "ExpertPlusStandard.dat");
 
